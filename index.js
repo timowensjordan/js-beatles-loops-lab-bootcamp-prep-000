bf325e090344363a72f68505bf135a8c8d9e2397
Tim Jordan <raeyejordan@yahoo.com>
2018-01-23 18:23:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ function johnLennonFacts(){
   const facts=["He was the last Beatle to learn to drive","He was never a vegetarian","He was a choir boy and boy scout","He hated the sound of his own voice"];
   var array=[];
   var i=0;
-  while
+  while (i<facts.length){
+    return array;
+  }
 }
 function iLoveTheBeatles(n){
   var array=[];
